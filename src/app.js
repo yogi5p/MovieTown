031109e3d59e96060f3896f40ce8e5d6a8f01098
Yogita Patil <nitinyogi@msn.com>
2018-03-28 18:47:30 -0700

    day 3 changes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import "jquery";
 import "./style.scss";
+import "bootstrap-loader";
 
 $(document).ready(function() {
   //create a function to fetch movies
@@ -9,7 +10,7 @@ $(document).ready(function() {
         "https://api.themoviedb.org/3/search/movie?api_key=2434d246ec60c162a86db597467ef4ed&language=en-US&query=princess&include_adult=false&sort_by=created_at&page=1",
       success: function(payload) {
         console.log(payload);
-        createMovieBoxes(payload);
+        createPosters(payload.results);
         //call a function that passes an array to it
         //that function will iterate over the array, creating a div with it
         //child img tag, the src of which will be the poster_path
@@ -17,32 +18,63 @@ $(document).ready(function() {
     });
   }
 
-  let containerDiv = $("<div>").attr("class", "container");
+  //create a function to fetch movies
+  function fetchMovieDetails(movie, movieId) {
+    console.log(movieId);
+    let httpRequestUrl =
+      "https://api.themoviedb.org/3/movie/" +
+      movieId +
+      "?api_key=2434d246ec60c162a86db597467ef4ed";
+    console.log(httpRequestUrl);
+    $.ajax({
+      url: `https://api.themoviedb.org/3/movie/${movieId}?api_key=2434d246ec60c162a86db597467ef4ed`,
+      success: movieDetails => {
+        console.log(movieDetails);
+        //$("#movieModal").modal();
+        presentMovieDetailsModal(movie);
+        //call a function that passes an array to it
+        //that function will iterate over the array, creating a div with it
+        //child img tag, the src of which will be the poster_path
+      }
+    });
+  }
 
+  //Present Modal for Movie!!!
+  const presentMovieDetailsModal = movie => {
+    $(".modal-title:first").text(movie.title);
+    $(".movieDetails-overview:first").text(movie.overview);
+    $(".movieDetails-img:first").attr(
+      "src",
+      "https://image.tmdb.org/t/p/w500/" + movie.poster_path
+    );
+    $(".movieDetails-tagline:first").text(movie.tagline);
+    $("#movieDetails-modal").modal("show");
+  };
+
+  let containerDiv = $("<div>").attr("class", "container");
   let rowDiv = $("<div>").attr("class", "row");
 
-  function createMovieBoxes(movieListPayload) {
-    let movieList = movieListPayload.results;
+  //Create our function to build and display the posters
+  function createPosters(moviesList) {
+    moviesList.forEach(function(movie) {
+      let movieColDiv = $("<div>").attr("class", "card col-md-3 m-3");
+      let movieImage = $("<img>")
+        .attr("src", "https://image.tmdb.org/t/p/w500/" + movie.poster_path)
+        .attr("class", "card-img-top");
 
-    for (let i = 0; i < movieList.length; i++) {
-      //<columnDiv outer with class="col-md-4">
-      var columnDiv = $("<div>").attr("class", "card col-md-4");
-      //<columnDiv inner with class="box-bg">
-      var boxDiv = $("<div>").attr("class", "box-bg");
+      let posterBody = $("<div>")
+        .attr("class", "card-body")
+        .append($("<h5>").html(movie.title))
+        .append(
+          $("<button>")
+            .html("Details")
+            .attr("class", "btn btn-info")
+            .click(() => fetchMovieDetails(movie, movie.id))
+        );
 
-      //img item for each box
-      var img = $("<img>").attr("class", "card-img-top");
-      let movieName =
-        "https://image.tmdb.org/t/p/w500/" + movieList[i].poster_path;
-      img.attr("src", movieName);
-
-      boxDiv.append(img).append($("<h5>").html(movieList[i].title));
-
-      boxDiv.appendTo(columnDiv);
-      columnDiv.appendTo(rowDiv);
-      //columnDiv.append(boxDiv);
-      //rowDiv.append(columnDiv);
-    }
+      movieColDiv.append(movieImage).append(posterBody);
+      rowDiv.append(movieColDiv);
+    });
   }
   rowDiv.appendTo(containerDiv);
   containerDiv.appendTo("body");
